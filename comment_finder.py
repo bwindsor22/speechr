22b318c85810eefdc11a0c893f8f2ce56865882a
Brad Windsor <bradwindsor@Brads-MacBook-Pro.local>
2018-01-07 22:38:02 -0500

    updating crawler

---
+++ comment_finder.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  7 21:37:37 2018
+
+@author: bradwindsor
+"""
+
---
+++ crawler.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  7 21:35:55 2018
+
+"""
+import praw
+import itertools
+import os
+import comment_classifier
+import hate_subreddit_finder
+import csv
+
+
+class Crawler:
+    def __init__(self):
+        self.reddit = praw.Reddit('bot1')
+        slurs = self.load_csv_resource('racial_slurs')
+        self.CC = comment_classifier.CommentClassifier(slurs)
+        subreddits_to_scan = self.load_csv_resource('policing_subreddits')
+        subreddits_to_scan = list(itertools.chain.from_iterable(subreddits_to_scan))
+
+        self.HRS = hate_subreddit_finder.HateSubredditFinder(self.reddit, subreddits_to_scan)
+        
+        
+    def load_csv_resource(self, file_name):
+        app_path = os.path.dirname(os.path.abspath(__file__))
+        slurs_file = os.path.join(os.path.sep, app_path, 'ref', file_name) + '.csv'
+        slurs = []
+        with open(slurs_file, 'r') as file:
+            rdr = csv.reader(file)
+            for row in rdr:
+                slurs.append(row)
+        return slurs
+                
+    def collect(self):
+        print('collecting...')
+        i = 0
+        
+        hate_subs = self.HRS.find_hate_subreddits(10)
+        print('hate subs: ' + str(hate_subs))
+        
+        for hate_sub in hate_subs:
+            print('--------------------------')
+            print('scanning next sub: ' + hate_sub)
+            print('--------------------------')
+
+            subreddit = self.reddit.subreddit(hate_sub)
+            submissions = list(subreddit.new(limit=5))
+            for sub in submissions:
+                sub.comments.replace_more(limit=0)
+                for comment in sub.comments.list():
+                    i += 1
+                    if i % 100 == 0:
+                        print('processing comment # ' + str(i))
+                    score = self.CC.analyze(comment)
+                    if score > 0:
+                        print(comment.body)
+                        print('score: ' + str(score))
+        
+
+
+if __name__ == '__main__':
+    c = Crawler()
+    c.collect()
