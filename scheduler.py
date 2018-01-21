e6198f22691a2c56a0f0e5dc28ead7f5135a2a13
CL <linchristopher24@gmail.com>
2018-01-21 12:42:09 -0800

    attempt 2 at scheduler

--- scheduler.py
+++ scheduler.py
@@ -3,22 +3,43 @@
 Created on Fri Jan 19 18:52:21 2018
 
 @author: CSL
+reference https://schedule.readthedocs.io/en/stable/api.html#main-interface
+******************************************************************************
+Important Notes:
+Each time schedule.run_pending is executed, everey task that is planned using
+the schedule.ever() is added to schedule.jobs.  Be sure to clear schedule.jobs
+before you run to make sure duplicate jobs aren't running.
+
+schedule.jobs = []
+Default Jobs list
+
+schedule.clear(tag=None)[source]
+Calls clear on the default scheduler instance.
+
+schedule.cancel_job(job)[source]
+Calls cancel_job on the default scheduler instance.
+
+schedule.next_run()[source]
+Calls next_run on the default scheduler instance.
+
 """
 
 import schedule
 import time # keep for when we want to schedule program to run at a certain time
 import crawler
 
+# schedule.clear()
+
 # define all the tasks you want to do
 def tasks_to_run():
     c = crawler.Crawler()
     c.run()
-
-#Create a schedule.every() for every task you want to run.
+    
 schedule.every(1).hour.do(tasks_to_run)
 
-# this actually executes the schedule class.
-for _ in range(0,9999):
+"""schedule.run_pending() checks which jobs are to be run and then runs the task
+that is scheduled each time it is called.  The infinite loop basically keeps
+allows the class to check keep checking when it needs to be run then runs it"""
+while True:
     schedule.run_pending()
-    # time.sleep(2)
-    
+    time.sleep(20)
