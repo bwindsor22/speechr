05c6a29cf8b8a826d54d4fdb2abc6dc0fd6d5567
CL <linchristopher24@gmail.com>
2018-01-21 11:53:51 -0800

    working scheduler with no logging

--- scheduler.py
+++ scheduler.py
@@ -6,16 +6,19 @@ Created on Fri Jan 19 18:52:21 2018
 """
 
 import schedule
-import time
+import time # keep for when we want to schedule program to run at a certain time
 import crawler
 
+# define all the tasks you want to do
+def tasks_to_run():
+    c = crawler.Crawler()
+    c.run()
 
-# c = crawler.Crawler()
-def test():
-    print("chris")
+#Create a schedule.every() for every task you want to run.
+schedule.every(1).hour.do(tasks_to_run)
 
-schedule.every(1).minute.do(test)
-
-for _ in range(0,999999):
+# this actually executes the schedule class.
+for _ in range(0,9999):
     schedule.run_pending()
-    time.sleep(3)
+    # time.sleep(2)
+    
